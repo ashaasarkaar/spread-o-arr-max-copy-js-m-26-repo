0a70ccda66ead9f9ab5c4f54193a38a326f62021
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-04 00:19:23 +0600

    copy array ba copy object onno kono array ta object korte hole, sei khettre properly copy hoi na, karon same reference dui ta ak e thake, tai ak tate change korle onno ta te o hoye jai, r ai plm dur korar jonno spread operator dorkar

--- spread.js
+++ spread.js
@@ -14,6 +14,20 @@ arraySum(array);  // this is wrong concept
 
 //spread operator wrong concept
 const array2 = [1, 4, 5];
-const arraySum2 = (a, b, c) => a+b+c;
+const arraySum2 = (a, b, c) =>{
+    const sum = a+b+c;
+    console.log(a, b,c);
+    return sum;
+}
 
-console.log(arraySum2(...array2));     // spread operator niye kaj korle of course console age variable a nite hobe;
+
+console.log(arraySum2(...array2));     // spread operator niye kaj korle of course console ar  age variable a nite hobe;
+
+//copy array ba copy object onno kono array ta object korte hole, sei khettre properly copy hoi na, karon same reference dui ta ak e thake, tai ak tate change korle onno ta te o hoye jai, r ai plm dur korar jonno spread operator dorkar
+
+// wrong way to copy array
+const arr1 = [1, 2, 3];
+const arr2 = arr1;
+arr2.push(5);
+console.log(arr1);  
+console.log(arr2);  // reference same thakar karone duitatei 4 add hobe, reference same mane dui ta ak e jaiga ba box a ase, tai j dik thekei value asuk na keno dui tatei change hobe
