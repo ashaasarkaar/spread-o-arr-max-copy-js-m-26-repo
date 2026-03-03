97b95d6bf798e3ab4036e6ced4f241630df38e95
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-03 23:48:59 +0600

    min + max function built in

---
+++ spread.js
@@ -0,0 +1,7 @@
+//min + max function built in
+const max = Math.max(1, 5, 7, 8,10);   // buit-in max function
+console.log(max);
+const min = Math.min(2,4,6,7,80);  // built-in min function
+console.log(min);
+
+
